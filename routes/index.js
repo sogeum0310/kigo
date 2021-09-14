259b36d1dcd276aaca6e6a15370a3ab84d1a4192
김대현 <gimdaehyeon@gimdaehyeon-ui-Macmini.local>
2021-09-14 12:27:07 +0900

    some

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ router.get('/estimates', function (req, res, next) {
 
   // doSomething(doSomethingElse)
 
-  async.parallel({ // 안녕 랩탑
+  async.parallel({ // 안녕 랩탑, 안녕 데스크탑
     estimates: function (callback) {
       Model.Estimate.find().exec(callback)
     }

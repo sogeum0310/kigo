8d817c03d08b7086b6e1577e1d81fd50a21b6fcf
tvvmvn <tvvmvn@gmail.com>
2021-09-14 12:26:04 +0900

    some

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ router.get('/estimates', function (req, res, next) {
 
   // doSomething(doSomethingElse)
 
-  async.parallel({
+  async.parallel({ // 안녕 랩탑
     estimates: function (callback) {
       Model.Estimate.find().exec(callback)
     }

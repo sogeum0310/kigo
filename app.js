f87561e2cb9e3e75da70dd36549d2940b89c3ea2
tvvmvn <tvvmvn@gmail.com>
2021-11-17 18:44:35 +0900

    air

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const mongoose = require('mongoose');
 const config = require('./config.js')
 const populate = require('./populate/populate')
 
-// What's going on
+// 11-17 수요일 6시 44분
 const mongoUrl = config.mydb.url
 // mongoose connection 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });

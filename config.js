ad97c4b3f77be4b3c9946670a15769b836c47ab3
EC2 Default User <ec2-user@ip-172-31-43-222.ap-northeast-2.compute.internal>
2021-11-19 07:15:59 +0000

    aws

--- config.js
+++ config.js
@@ -1,16 +1,25 @@
+<<<<<<< HEAD
 var local_url = 'http://127.0.0.1:3000' 
 var aws_url = 'http://ec2-15-164-219-91.ap-northeast-2.compute.amazonaws.com:3000'
 
 
+=======
+>>>>>>> c2dec00f (aws)
 // Database connection
 exports.mydb = {
   name: 'mongodb',
   // url: 'mongodb://localhost:27017/local_kigo',
+<<<<<<< HEAD
   // url: 'mongodb://sogeum0310:hyun0831**@ec2-15-164-219-91.ap-northeast-2.compute.amazonaws.com:27017/test?authSource=admin&authMechanism=SCRAM-SHA-1',
   url: 'mongodb://sogeum0310:hyun0831**@ec2-15-164-219-91.ap-northeast-2.compute.amazonaws.com:27017/test?authSource=admin&authMechanism=SCRAM-SHA-1'
 }
 
 
+=======
+  url: 'mongodb://sogeum0310:hyun0831**@ec2-15-164-219-91.ap-northeast-2.compute.amazonaws.com:27017/test?authSource=admin&authMechanism=SCRAM-SHA-1'
+}
+
+>>>>>>> c2dec00f (aws)
 // Mailing account
 exports.account = {
   email: 'sogeum0310@gmail.com',
@@ -36,5 +45,9 @@ exports.ids = {
     clientSecret: '',
     callbackURL: "http://ec2-15-164-219-91.ap-northeast-2.compute.amazonaws.com:3000/auth/kakao/callback"
   }
+<<<<<<< HEAD
 };
 
+=======
+};
+>>>>>>> c2dec00f (aws)
